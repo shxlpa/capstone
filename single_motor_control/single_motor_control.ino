7e5e2287597085b02f660e9201ab5cae719a7d5d
raoshilpa <shilpa.padiki.rao@gmail.com>
2024-02-07 12:56:39 -0800

    driver control

---
+++ single_motor_control/single_motor_control.ino
@@ -0,0 +1,15 @@
+#include "DualG2HighPowerMotorShield.h"
+
+
+// DualG2HighPowerMotorShield24v14 md
+DualG2HighPowerMotorShield18v18 md;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
