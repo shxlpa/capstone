01df4c127a0a335c2e753c7fe88b50f32a81780e
raoshilpa <shilpa.padiki.rao@gmail.com>
2024-02-25 19:08:40 -0800

    updating files mux

--- teensy_single_nmsensor/teensy_single_nmsensor.ino
+++ teensy_single_nmsensor/teensy_single_nmsensor.ino
@@ -2,7 +2,7 @@
 #include "MS5837.h"
 
 MS5837 sensor;
-MS5837 sensor1;
+// MS5837 sensor1;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,37 +14,26 @@ void setup() {
   Wire.begin();
   Wire.setSDA(18);
   Wire.setSCL(19);
-  Wire1.begin();
-  Wire1.setSDA(17);
-  Wire1.setSCL(16);
- 
-  sensor.init(Wire);
+
+  while (!sensor.init()) {
+    Serial.println("Init failed!");
+    Serial.println("Are SDA/SCL connected correctly?");
+    Serial.println("Blue Robotics Bar30: White=SDA, Green=SCL");
+    Serial.println("\n\n\n");
+    delay(5000);
+  }
+
   sensor.setModel(MS5837::MS5837_02BA);
   sensor.setFluidDensity(1.2); // kg/m^3 (freshwater, 1029 for seawater)
-  
-  sensor1.init(Wire1);
-  sensor1.setModel(MS5837::MS5837_02BA);
-  sensor1.setFluidDensity(1.2); // kg/m^3 (freshwater, 1029 for seawater)
-  
-  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensor.read();
-  //Serial.print("Pressure 0: "); 
-  Serial.print(sensor.pressure()); 
-  //Serial.println(" mbar");
-  Serial.print(",");
 
-  sensor1.read();
-  //Serial.print("Pressure 1: "); 
-  Serial.print(sensor1.pressure()); 
-  //Serial.println(" mbar");
-  Serial.println();
-  //Serial.print("Temperature: "); 
-  //Serial.print(sensor.temperature()); 
-  //Serial.println(" deg C");
+  Serial.println(sensor.pressure()); 
+  // Serial.println("new line!");
 
   delay(1);
 }
